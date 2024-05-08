c38ed54614719f9cc0accd57eecc5374bd19b757
Revathi2306 <76587434+Revathi2306@users.noreply.github.com>
2024-05-08 15:45:56 +0530

    seller routes

--- src/pages/Auth/Signup.js
+++ src/pages/Auth/Signup.js
@@ -10,7 +10,13 @@ const Signup = () => {
   const [password, setPassword] = useState("");
   const [phone, setPhone] = useState("");
   const [user_type, setUserType] = useState("");
-
+  const [domain, setDomain] = useState("");
+  const [getProducts, setgetProducts] = useState("");
+  const [getProductDetail, setgetProductDetail] = useState("");
+  const [getOrder, setgetOrder] = useState("");
+  const [getPlaceOrder, setPlaceOrder] = useState("");
+  const [CancelOrder, setCancelOrder] = useState("");
+ 
   const handleSignup = async (e) => {
     console.log("b4 fetch", name , user_type)
     e.preventDefault();
@@ -25,6 +31,12 @@ const Signup = () => {
           password: password,
           phone: phone,
           userType: user_type,
+          domain: domain,
+          getProduct: getProducts,
+          getOrder: getOrder,
+          placeOrder: getPlaceOrder,
+          cancelOrder: CancelOrder,
+          getIndivisualProduct: getProductDetail
         }),
       headers: {
         "Content-type": "application/json; charset=UTF-8"
@@ -48,11 +60,10 @@ const Signup = () => {
     }
   }
 
-
   return (
     <>
     <div className='container auth-div'>
-    <div className='col-md-5 auth-box'>
+    <div className='col-md-7 auth-box'>
     <h4 className='heading'> Signup </h4>
     <form onSubmit={handleSignup}>
           <div className="mb-3">
@@ -112,11 +123,94 @@ const Signup = () => {
             /> */}
 
             <select className="form-select form-control" aria-label="Default select example" 
-              onChange={(e) => setUserType(e.target.value)} placeholder="Select Application Service type" >
+              onChange={(e) => {
+                e.preventDefault();
+                setUserType(e.target.value);
+              }} placeholder="Select Application Service type" >
               <option className="diabled-option-signup" disabled selected value>select application service</option>
               <option value={"buyer"} className="option-signup">Buyer Application</option>
               <option value={"seller"} className="option-signup">Seller Application</option>
             </select>
+
+            {(()=>{
+              if(user_type == "seller"){
+                return(
+                <div className='text-start'>
+                <label htmlFor="ProductformQuantity" className="form-label signup-form-label">
+                  Domain
+                </label>
+                <input
+                  type="text"
+                  placeholder="Domain of Your App"
+                  className="form-control signup-form-input"
+                  onChange={(e) => setDomain(e.target.value)}
+                />
+                <div className='row'> 
+                <div className='col'>
+                  <label htmlFor="ProductformQuantity" className="form-label signup-form-label">
+                    Get All Product
+                  </label>
+                  <input
+                    type="text"
+                    placeholder="path for all products"
+                    className="form-control signup-form-input"
+                    onChange={(e) => setgetProducts(e.target.value)}
+                  />
+                </div>
+                <div className='col'>
+                  <label htmlFor="ProductformQuantity" className="form-label signup-form-label">
+                    Get Product Detail
+                  </label>
+                  <input
+                    type="text"
+                    placeholder="path for product detail"
+                    className="form-control signup-form-input"
+                    onChange={(e) => setgetProductDetail(e.target.value)}
+                  />
+                </div>
+                </div>
+
+                <div className='row'> 
+                <div className='col'>
+                  <label htmlFor="ProductformQuantity" className="form-label signup-form-label">
+                    Get Order
+                  </label>
+                  <input
+                    type="text"
+                    placeholder="path for order detail"
+                    className="form-control signup-form-input"
+                    onChange={(e) => setgetOrder(e.target.value)}
+                  />
+                </div>
+                <div className='col'>
+                  <label htmlFor="ProductformQuantity" className="form-label signup-form-label">
+                    Place Order
+                  </label>
+                  <input
+                    type="text"
+                    placeholder="path for placing order"
+                    className="form-control signup-form-input"
+                    onChange={(e) => setPlaceOrder(e.target.value)}
+                  />
+                </div>
+                <div className='col'>
+                  <label htmlFor="ProductformQuantity" className="form-label signup-form-label">
+                    Cancel Order
+                  </label>
+                  <input
+                    type="text"
+                    placeholder="path for cancelling order"
+                    className="form-control signup-form-input"
+                    onChange={(e) => setCancelOrder(e.target.value)}
+                  />
+                </div>
+                </div>
+
+
+                </div>)
+              }
+            })()}
+
           </div>
 
           
